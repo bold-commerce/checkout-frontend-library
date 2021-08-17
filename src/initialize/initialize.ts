e43758fa6da43bc01f4a0182eade9acdac137536
Felipe Dos Santos <felipe.dossantos@boldcommerce.com>
2021-08-17 15:12:49 -0500

    CE-180: Improve initialize function

--- src/initialize/initialize.ts
+++ src/initialize/initialize.ts
@@ -1,11 +1,11 @@
 import {
-    IApiReturnObject, 
-    IInitializeOrderResponse, 
-    IEnvironment, 
-    setEnvironment, 
-    sessionStart, 
-    setJwtToken, 
-    setPublicOrderId, 
+    IApiReturnObject,
+    IInitializeOrderResponse,
+    IEnvironment,
+    setEnvironment,
+    sessionStart,
+    setJwtToken,
+    setPublicOrderId,
     setShopIdentifier,
     checkApiResponse,
 } from 'src';
@@ -13,23 +13,23 @@ import {baseReturnObject, keysToTestFromResponse} from 'src/variables';
 
 /**
  * # Initialize
- * 
+ *
  * Initializes a checkout session using a variety of environment variables
- * 
+ *
  * @param initData initial data from /init API call
- * @param jwt Json web token which will be traded for a CSRF token
- * @param publicOrderId Order ID for the order
  * @param shopIdentifier Identification for the shop in which the order is on
+ * @param environment Set the environment where the library will be working on.
  */
-export async function initialize(initData: IInitializeOrderResponse, jwt: string, publicOrderId: string, shopIdentifier: string, environment: IEnvironment): Promise<IApiReturnObject> { 
+export async function initialize(initData: IInitializeOrderResponse, shopIdentifier: string, environment: IEnvironment): Promise<IApiReturnObject> {
+    const {jwt_token: jwt, public_order_id: publicOrderId} = initData;
     const returnObject = {...baseReturnObject};
     returnObject.success = true;
     returnObject.response = initData;
     const keysToCheck = [keysToTestFromResponse.applicationState, keysToTestFromResponse.initial_data, keysToTestFromResponse.jwt_token, keysToTestFromResponse.public_order_id];
-    const returnValue = checkApiResponse(returnObject, keysToCheck); 
-    if(!returnValue.success) { 
+    const returnValue = checkApiResponse(returnObject, keysToCheck);
+    if(!returnValue.success) {
         return returnValue;
-    } 
+    }
     setJwtToken(jwt);
     setPublicOrderId(publicOrderId);
     setShopIdentifier(shopIdentifier);
