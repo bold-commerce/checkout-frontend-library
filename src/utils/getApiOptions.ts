0d33098e6e4d1e8d4d0f589639b50d22641067b2
Felipe Dos Santos <felipe.dossantos@boldcommerce.com>
2021-08-26 16:49:27 -0500

    CE-139: Fix to have no body for GET requests

--- src/utils/getApiOptions.ts
+++ src/utils/getApiOptions.ts
@@ -6,14 +6,15 @@ import {apiTypes, methods} from 'src/variables';
  * returns a formatted object that'll represent the 'options' for a fetch api call
  *
  * @param type type of api that needs to be constructed
- * @param body object for body of request
+ * @param b object for body of request
  */
-export function getApiOptions(type: keyof IApiTypes, body: IGetApiOptionsBody = {}): RequestInit {
+export function getApiOptions(type: keyof IApiTypes, b: IGetApiOptionsBody = {}): RequestInit {
     const { method, useCsrf } = apiTypes[type] ?? {method: methods.GET};
+    const body: BodyInit | null = b && Object.keys(b).length === 0 ? null : JSON.stringify(b);
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     if(useCsrf) {
         headers.append('X-CSRF-TOKEN', getCsrfToken());
     }
-    return { method, headers, body: JSON.stringify(body) };
+    return { method, headers, body };
 }
