265dba51a2a25dc6ff7853fc3dd267794185d9ec
Felipe dos Santos <felipe.dossantos@boldcommerce.com>
2023-05-05 20:31:37 +0000

    Update library to publish on public NPM Registry

--- tests/externalPaymentGateway/actionsToIframe/sendExternalPaymentGatewayUpdateLanguageAction.test.ts
+++ tests/externalPaymentGateway/actionsToIframe/sendExternalPaymentGatewayUpdateLanguageAction.test.ts
@@ -1,10 +1,8 @@
 import {
     sendExternalPaymentGatewayUpdateLanguageAction,
     IExternalPaymentGateway,
-    IInitializeOrderResponse
 } from 'src';
 import * as sendExternalPaymentGatewayActionToIframe from 'src/externalPaymentGateway/actionsToIframe/sendExternalPaymentGatewayActionToIframe';
-import {initializeOrderResponseMock} from 'src/variables/mocks';
 
 describe('testing send external payment gateway update language Action', () => {
     let sendExternalPaymentGatewayActionToIframeSpy: jest.SpyInstance;
@@ -12,7 +10,6 @@ describe('testing send external payment gateway update language Action', () => {
     const gateway: IExternalPaymentGateway = {
         base_url: '', iframe_url: '', is_test: false, location: '', public_id: '', target_div: '', currency: ''
     };
-    const state: IInitializeOrderResponse = initializeOrderResponseMock;
 
     beforeEach(() => {
         jest.restoreAllMocks();
