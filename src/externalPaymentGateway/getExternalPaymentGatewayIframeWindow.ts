40a9f059829e1acde264891e4e4a2254aa4f31cb
Nima Sheibani <nima.sheibani@boldcommerce.com>
2023-08-15 11:47:39 -0500

    PXN-1048: fix external payment iframe not loading

--- src/externalPaymentGateway/getExternalPaymentGatewayIframeWindow.ts
+++ src/externalPaymentGateway/getExternalPaymentGatewayIframeWindow.ts
@@ -4,7 +4,7 @@ import {IExternalPaymentGateway} from 'src';
  * Get the contentWindow of the external payment gateway iFrame
  */
 export function getExternalPaymentGatewayIframeWindow(externalPaymentGateway: IExternalPaymentGateway): WindowProxy | null {
-    const selector = `iframe#${externalPaymentGateway.target_div}`;
+    const selector = `iframe#${externalPaymentGateway.public_id}`;
     const iframeElement: HTMLIFrameElement | null = document.querySelector(selector);
     if (iframeElement) {
         return iframeElement.contentWindow || null;
