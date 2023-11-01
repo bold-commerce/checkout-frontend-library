e028c5ccaf89dd807082df7ff35d4b9fa1967385
Joshua <joshua.simard@boldcommerce.com>
2023-11-01 20:28:08 +0000

    PXN-1329: Fix Syntax Error in EPG

--- src/externalPaymentGateway/getExternalPaymentGatewayIframeWindow.ts
+++ src/externalPaymentGateway/getExternalPaymentGatewayIframeWindow.ts
@@ -4,8 +4,7 @@ import {IExternalPaymentGateway} from 'src';
  * Get the contentWindow of the external payment gateway iFrame
  */
 export function getExternalPaymentGatewayIframeWindow(externalPaymentGateway: IExternalPaymentGateway): WindowProxy | null {
-    const selector = `iframe#${externalPaymentGateway.public_id}`;
-    const iframeElement: HTMLIFrameElement | null = document.querySelector(selector);
+    const iframeElement = <HTMLIFrameElement|null> document.getElementById(externalPaymentGateway.public_id);
     if (iframeElement) {
         return iframeElement.contentWindow || null;
     }
