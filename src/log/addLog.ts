86b5542d363c4e2ee399f8241193f51ed80fe8c0
Felipe dos Santos <felipe.dossantos@boldcommerce.com>
2023-11-14 13:32:15 -0600

    Update addLog

--- src/log/addLog.ts
+++ src/log/addLog.ts
@@ -5,16 +5,17 @@ import {apiTypeKeys, apiTypes, IApiReturnObject, checkApiResponse, fetchAPI, get
  *
  * This make requests which contain messages that will be logged through the usual Checkout logging mechanisms.
  *
- * @param message Message to add to Logs - Maximum of 100 Characters
+ * @param message Message to add to Logs - Maximum of 200 Characters
  * @param key Optional Key to add to NR counters
+ * @param details stringify Json object to add to log context - Maximum of 700 Characters
  * @param numOfRetries
  *
  */
-export async function addLog(message: string, key?: string, numOfRetries = 0): Promise<IApiReturnObject> {
+export async function addLog(message: string, key?: string, details?: string, numOfRetries = 0): Promise<IApiReturnObject> {
     const {addLog} = apiTypeKeys;
     const {keysToTest} = apiTypes.addLog;
     const url = getApiUrl(addLog);
-    const options = getApiOptions(addLog, { message, key });
+    const options = getApiOptions(addLog, { message, key, details });
     const fetchRes = await fetchAPI(url, options, numOfRetries);
     return checkApiResponse(fetchRes, keysToTest);
 }
